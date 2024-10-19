6360adac1cf3427f2903a71144a2e8fb8e291522
Kokooch <kkokochinchaladze@gmail.com>
2024-10-19 16:25:24 +0400

    Add ui project

---
+++ Projects/KoggoInvestments.Ui/KoggoInvestments.Ui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KoggoInvestments.Ui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Projects/KoggoInvestments.Ui/KoggoInvestments.Ui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KoggoInvestments.Ui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
