72bf0cb0432066561ce6610cff4e9724590cd426
Kokooch <kkokochinchaladze@gmail.com>
2024-10-19 16:37:40 +0400

    working version

--- Projects/KoggoInvestments.Ui/KoggoInvestments.Ui/App.xaml.cs
+++ Projects/KoggoInvestments.Ui/KoggoInvestments.Ui/App.xaml.cs
@@ -1,12 +1,30 @@
-﻿namespace KoggoInvestments.Ui
+﻿using KoggoInvestments.UI.Services;
+
+namespace KoggoInvestments.Ui
 {
     public partial class App : Application
     {
+        private PeriodicService _periodicService;
+
         public App()
         {
             InitializeComponent();
 
             MainPage = new AppShell();
+            _periodicService = new PeriodicService();
+        }
+
+        protected override void OnStart()
+        {
+            _periodicService.StartService();
+        }
+        protected override void OnSleep()
+        {
+            _periodicService.StopService();
+        }
+        protected override void OnResume()
+        {
+            _periodicService.StartService();
         }
     }
 }
