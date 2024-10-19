bc68da1529d85308f5d5d1db95fd36b534f32492
Kokooch <kkokochinchaladze@gmail.com>
2024-10-19 18:40:26 +0400

    test 1

---
+++ Projects/KoggoInvestments.Ui/KoggoInvestments.Ui/NotificationEventArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace KoggoInvestments.Ui;
+
+public class NotificationEventArgs : EventArgs
+{
+    public string Title { get; set; }
+    public string Message { get; set; }
+}
+
--- Projects/KoggoInvestments.Ui/KoggoInvestments.Ui/Platforms/Android/MainActivity.cs
+++ Projects/KoggoInvestments.Ui/KoggoInvestments.Ui/Platforms/Android/MainActivity.cs
@@ -1,11 +1,38 @@
-﻿using Android.App;
-using Android.Content.PM;
-using Android.OS;
+﻿    using Android.App;
+    using Android.Content;
+    using Android.Content.PM;
+    using Android.OS;
+    using KoggoInvestments.Ui.Notifications;
 
-namespace KoggoInvestments.Ui
-{
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-    public class MainActivity : MauiAppCompatActivity
+    namespace KoggoInvestments.Ui
     {
+        [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+        public class MainActivity : MauiAppCompatActivity
+        {
+            protected override void OnCreate(Bundle? savedInstanceState)
+            {
+                base.OnCreate(savedInstanceState);
+
+                CreateNotificationFromIntent(Intent);
+            }
+
+            protected override void OnNewIntent(Intent? intent)
+            {
+                base.OnNewIntent(intent);
+
+                CreateNotificationFromIntent(intent);
+            }
+
+            static void CreateNotificationFromIntent(Intent intent)
+            {
+                if (intent?.Extras != null)
+                {
+                    string title = intent.GetStringExtra(Platforms.Android.NotificationManagerService.TitleKey);
+                    string message = intent.GetStringExtra(Platforms.Android.NotificationManagerService.MessageKey);
+
+                    var service = IPlatformApplication.Current?.Services.GetService<INotificationManagerService>();
+                    service?.ReceiveNotification(title, message);
+                }
+            }
+        }
     }
-}
