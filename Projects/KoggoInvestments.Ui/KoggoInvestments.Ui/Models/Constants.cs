c2f3d110c6381d0f73e57fc70e367b63d274fe0b
Kokooch <kkokochinchaladze@gmail.com>
2024-10-20 11:45:40 +0400

    returned polling

--- Projects/KoggoInvestments.Ui/KoggoInvestments.Ui/Models/Constants.cs
+++ Projects/KoggoInvestments.Ui/KoggoInvestments.Ui/Models/Constants.cs
@@ -2,9 +2,10 @@
 {
     public static class Constants
     {
-        public static string LocalhostUrl = DeviceInfo.Platform == DevicePlatform.Android ? "10.0.2.2" : "localhost";
-        public static string Scheme = "https";
-        public static string Port = "7141";
+        public static string LocalhostUrl = DeviceInfo.Platform == DevicePlatform.Android ? "44.207.32.191" : "localhost";
+        public static string Scheme = "http";
+        public static string Port = "5017";
         public static string RestUrl = $"{Scheme}://{LocalhostUrl}:{Port}/Stocks/status";
     }
+  //  http://44.207.32.191:5017
 }
